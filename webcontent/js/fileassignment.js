87ff821379f856be0ae720fea7289b4404133152
sshivangi469 <50018798+sshivangi469@users.noreply.github.com>
2019-05-09 23:49:28 +0530

    Delete fileassignment.js

--- webcontent/js/fileassignment.js
+++
@@ -1,7 +0,0 @@
-
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
-  $http.get("https://jsonplaceholder.typicode.com/posts").then(function (response) {
-      $scope.myTable = response.data;
-  });
-});
