2ca0478a58db9989b4369cd21994ed2d2c96a96a
sshivangi469 <50018798+sshivangi469@users.noreply.github.com>
2019-05-09 23:45:38 +0530

    Update fileassignment.js

--- webcontent/js/fileassignment.js
+++ webcontent/js/fileassignment.js
@@ -2,6 +2,6 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
   $http.get("https://jsonplaceholder.typicode.com/posts").then(function (response) {
-      $scope.myWelcome = response.data;
+      $scope.myTable = response.data;
   });
 });
