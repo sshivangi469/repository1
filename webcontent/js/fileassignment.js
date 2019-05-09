18dea06d57e385539754efce9c50989e1d5ad6f0
sshivangi469 <50018798+sshivangi469@users.noreply.github.com>
2019-05-09 23:50:05 +0530

    Add files via upload

---
+++ webcontent/js/fileassignment.js
@@ -0,0 +1,7 @@
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+  $http.get("https://jsonplaceholder.typicode.com/posts").then(function (response) {
+      $scope.myTable = response.data;
+  });
+});
